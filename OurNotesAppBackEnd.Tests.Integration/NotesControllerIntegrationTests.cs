8439fdba31fdd3ceb6450425c8e0061104dd1853
Radzivon-Kliuchnikau <rodionk510@gmail.com>
2025-04-01 19:46:45 +0200

    ONPA-30: add integration tests infrastructure

--- OurNotesAppBackEnd.Tests.Integration/NotesControllerIntegrationTests.cs
+++ OurNotesAppBackEnd.Tests.Integration/NotesControllerIntegrationTests.cs
@@ -3,30 +3,38 @@ using System.Net.Http.Json;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using OurNotesAppBackEnd.Dtos;
+using OurNotesAppBackEnd.Tests.Integration.Infrastructure;
 
 namespace OurNotesAppBackEnd.Tests.Integration;
 
-public class NotesControllerIntegrationTests : IClassFixture<WebApiFactory>
+public class NotesControllerIntegrationTests : IClassFixture<IntegrationTestWebApiFactory>, IAsyncLifetime
 {
-    private readonly WebApiFactory _webApplicationFactory;
-
-    public NotesControllerIntegrationTests(WebApiFactory webApplicationFactory)
+    private readonly HttpClient _httpСlient;
+    // private Func<Task> _resetDatabase;
+    
+    public NotesControllerIntegrationTests(IntegrationTestWebApiFactory integrationTestWebApplicationFactory)
     {
-        _webApplicationFactory = webApplicationFactory;
+        _httpСlient = integrationTestWebApplicationFactory.HttpClient;
+        // _resetDatabase = integrationTestWebApplicationFactory.ResetDatabaseAsync;
     }
 
     [Fact]
     public async Task NotesController_GetAllNotes_Return_All_Notes_From_Database()
     {
         // Arrange
-        var httpClient = _webApplicationFactory.CreateClient();
-
         // Act
-        var response = await httpClient.GetAsync("api/notes");
+        var response = await _httpСlient.GetAsync("api/notes");
         var result = response.Content.ReadFromJsonAsync<NoteReadDto>(); 
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
         result.Should().NotBeNull();
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+
+    public async Task DisposeAsync()
+    {
+        // await _resetDatabase();
     }
 }
